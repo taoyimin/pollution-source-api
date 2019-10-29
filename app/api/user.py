296947f9a5699106c75c70217d56e32dead2a4cf
taoyimin <1103995288@qq.com>
2019-10-29 17:59:40 +0800

    regular update

--- app/api/user.py
+++ app/api/user.py
@@ -2,86 +2,77 @@
 # -*- coding: utf-8 -*-
 # Author:Tao Yimin
 # Time  :2019/4/28 17:16
-from flask_restful import reqparse, Resource, fields, marshal_with
+from flask import request, g
+from flask_restful import reqparse, Resource, fields, marshal_with, abort
 from werkzeug.routing import ValidationError
 
 from app.api import api
 from app.model import db, auth
 from app.model.user import User
-from app.util.common import valid_username, valid_password, valid_email, metric
+from app.util.common import metric, valid_user_name, valid_pass_word, valid_not_empty
 
-
-def username(username_str):
-    if valid_username(username_str):
-        return username_str
-    else:
-        raise ValidationError("账号不能小于6位，不能有空字符串")
-
-
-def password(password_str):
-    if valid_password(password_str):
-        return password_str
-    else:
-        raise ValidationError("密码不能小于6位，不能有空字符串")
-
-
-def email(email_str):
-    if valid_email(email_str):
-        return email_str
-    else:
-        raise ValidationError("输入的不是有效的邮箱！")
-
-
-user_fields = {
+user_item_fields = {
     'userId': fields.Integer,
     'userName': fields.String,
     'realName': fields.String,
-    'passWord': fields.String,
     'globalLevel': fields.String,
     'userLevel': fields.String,
-    'districts': fields.String(attribute=lambda user: ','.join(user.get_district_code_list())),
+    'orgId': fields.String,
+    'state': fields.Integer,
+    'districts': fields.String(attribute=lambda user: ','.join(map(lambda d: d.districtCode, user.districts)))
 }
 
-user_collection_fields = {
-    'total': fields.Integer,
-    'items': fields.List(fields.Nested(user_fields)),
-    'page': fields.Integer,
-    'per_page': fields.Integer,
-    'has_next': fields.Boolean
+user_list_fields = {
+    'total': fields.Integer(attribute=lambda pagination: pagination.total),
+    'currentPage': fields.Integer(attribute=lambda pagination: pagination.page),
+    'pageSize': fields.Integer(attribute=lambda pagination: pagination.per_page),
+    'hasNext': fields.Boolean(attribute=lambda pagination: pagination.has_next),
+    'list': fields.List(fields.Nested(user_item_fields), attribute=lambda pagination: pagination.items)
 }
 
 login_fields = {
-    'message': fields.String,
-    'token': fields.String,
-    'user': fields.Nested(user_fields)
+    'token': fields.String
 }
 
 
 class UserResource(Resource):
-    def __init__(self):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('username', type=username, help='用户名验证错误')
-        self.parser.add_argument('password', type=password, help='密码验证错误')
-        self.parser.add_argument('email', type=email)
-        self.parser.add_argument('nickname', type=str)
-        super(UserResource, self).__init__()
+    # def __init__(self):
+    #     self.parser = reqparse.RequestParser()
+    #     self.parser.add_argument('userName', type=valid_user_name)
+    #     self.parser.add_argument('passWord', type=valid_pass_word)
+    #     self.parser.add_argument('realName', default=None)
+    #     self.parser.add_argument('globalLevel', default=None)
+    #     self.parser.add_argument('userLevel', default=None)
+    #     super(UserResource, self).__init__()
 
     @metric
-    @marshal_with(user_fields)
-    def get(self, id=None, username=None):
-        if id is not None:
-            user = User.query.get(id)
+    @marshal_with(user_item_fields)
+    def get(self, id=None, user_name=None):
+        if id:
+            user = User.query.get_or_404(id, description='id=%d的用户不存在' % id)
         else:
-            user = User.query.filter_by(username=username).first()
+            user = User.query.filter_by(userName=user_name).first_or_404(description='用户名%s的用户不存在' % user_name)
         return user
 
     @metric
-    @marshal_with(user_fields)
+    @auth.login_required
+    @marshal_with(user_item_fields)
     def put(self, id):
-        args = self.parser.parse_args()
-        user = User.query.get(id)
+        user = User.query.get_or_404(id, description='id=%d的用户不存在' % id)
+        parser = reqparse.RequestParser()
+        parser.add_argument('userName', type=valid_user_name)
+        parser.add_argument('passWord', type=valid_pass_word)
+        parser.add_argument('realName')
+        parser.add_argument('orgId')
+        parser.add_argument('globalLevel')
+        parser.add_argument('userLevel')
+        args = parser.parse_args()
+        if g.user.userId != id:
+            abort(400, message='当前token与id=%d的用户不匹配，您只能修改当前token所属的用户信息' % id)
         for key, value in args.items():
             if value is not None:
+                if key == 'userName' and User.query.filter(User.userId != id, User.userName == value).scalar():
+                    abort(400, message='用户名%s已经存在' % value)
                 setattr(user, key, value)
         db.session.commit()
         return user
@@ -90,54 +81,77 @@ class UserResource(Resource):
 class UserCollectionResource(Resource):
 
     @metric
-    @marshal_with(user_collection_fields)
+    @marshal_with(user_list_fields)
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('page', type=int, default=1)
-        parser.add_argument('per_page', type=int, default=20)
+        parser.add_argument('currentPage', type=int, default=1)
+        parser.add_argument('pageSize', type=int, default=20)
         args = parser.parse_args()
-        pagination = User.query.paginate(args['page'], args['per_page'], False)
-        return {'total': pagination.total, 'page': pagination.page, 'per_page': pagination.per_page, 'has_next': pagination.has_next,
-                'items': pagination.items}
+        return User.query.paginate(args['currentPage'], args['pageSize'], False)
 
     @metric
-    @marshal_with(user_fields)
+    @marshal_with(user_item_fields)
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('username', type=username, required=True)
-        parser.add_argument('password', type=password, required=True)
-        parser.add_argument('email', type=email)
+        parser.add_argument('userName', required=True, type=valid_user_name)
+        parser.add_argument('passWord', required=True, type=valid_pass_word)
+        parser.add_argument('realName', required=True, type=valid_not_empty)
+        parser.add_argument('orgId', required=True, type=valid_not_empty)
+        parser.add_argument('globalLevel', required=True, type=valid_not_empty)
+        parser.add_argument('userLevel', required=True, type=valid_not_empty)
         args = parser.parse_args()
-        user = User()
-        for key, value in args.items():
-            if value is not None:
-                setattr(user, key, value)
-        db.session.add(user)
-        db.session.commit()
-        return user
+        user = User(**args)
+        if User.query.filter_by(userName=user.userName).scalar():
+            abort(400, message='用户名%s已经存在' % user.userName)
+        else:
+            db.session.add(user)
+            db.session.commit()
+            return user
 
 
-class UserLoginResource(Resource):
+class TokenResource(Resource):
     @metric
     @marshal_with(login_fields)
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('userName', type=username, required=True)
-        parser.add_argument('passWord', type=password, required=True)
+        parser.add_argument('userName', type=valid_user_name, required=True)
+        parser.add_argument('passWord', type=valid_pass_word, required=True)
         args = parser.parse_args()
-        user = User.query.filter_by(userName=args['userName']).first()
-        if user is None:
-            return {'message': '账号不存在'}, 400
+        user = User.query.filter_by(userName=args['userName']).first_or_404(description='用户名%s不存在' % args['userName'])
+        if user.passWord == args['passWord']:
+            token = user.generate_auth_token()
+            return {'token': token}
         else:
-            if user.passWord == args['passWord']:
-                # print(user)
-                token = user.generate_auth_token()
-                # print(token)
-                return {'message': '登录成功', 'token': token, 'user': user}, 200
-            else:
-                return {'message': '密码错误'}, 400
+            abort(400, message='密码错误')
 
 
-api.add_resource(UserResource, '/users/<int:id>', '/users/<string:username>')
+class IndexResource(Resource):
+    decorators = [auth.login_required]
+
+    @metric
+    def get(self):
+        return {
+            'code': 1,
+            'message': 'success',
+            'data':
+                {
+                    "34": "1, 6, 80, 96, 88.89",
+                    "90": "1,1,1",
+                    "80": "1, 1,1,1",
+                    "70": "1,5,1,1,1,1,1",
+                    "60": "1, 1643, 2474,441",
+                    "50": "1,20,18,19",
+                    "40": "1,3370,198,257,120,100,37,124,170,107",
+                    "30": "1, 16, 93.75, 92.75, 100",
+                    "20": "1,41, 28, 30",
+                    "31": "1, 10, 100, 100, 88.89",
+                    "10": "1,南昌,2019-08-10 09,67,良,PM2.5,80,72,12,22,20,0.32",
+                    "21": "1, 91.4, 96.19, 202"
+                }
+        }
+
+
+api.add_resource(UserResource, '/users/<int:id>', '/users/<string:user_name>')
 api.add_resource(UserCollectionResource, '/users')
-api.add_resource(UserLoginResource, '/login')
+api.add_resource(TokenResource, '/token')
+api.add_resource(IndexResource, '/index')
