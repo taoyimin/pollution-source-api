7f2891667e50be4c0f469caa1f8689693c9ec1ce
taoyimin <1103995288@qq.com>
2019-11-05 18:50:47 +0800

    regular update

--- app/api/discharge.py
+++ app/api/discharge.py
@@ -2,59 +2,70 @@
 # -*- coding: utf-8 -*-
 # Author:Tao Yimin
 # Time  :2019/10/22 19:23
-from flask import g
-from flask_restful import marshal_with, Resource, fields, reqparse, abort
+from flask_restful import marshal_with, Resource, fields, reqparse
 
 from app.api import api
-from app.api.enter import enter_detail_fields
 from app.model import auth
 from app.model.discharge import Discharge
 from app.model.enter import Enter
 from app.model.monitor import Monitor
-from app.util.common import metric, filter_none
+from app.model.report import Report
+from app.util.common import metric
 
 discharge_detail_fields = {
-    'dischargeId': fields.Integer,
+    'dischargeId': fields.String,
+    'enterId': fields.String,
     'dischargeName': fields.String,
+    'dischargeShortName': fields.String,
     'dischargeAddress': fields.String,
-    'enter': fields.Nested(enter_detail_fields)
+    'dischargeNumber': fields.String,
+    'dischargeTypeStr': fields.String,
+    'denoterInstallTypeStr': fields.String,
+    'dischargeRuleStr': fields.String,
+    'outletTypeStr': fields.String,
+    'longitude': fields.String,
+    'latitude': fields.String
 }
 
 discharge_item_fields = {
-    'dischargeId': fields.Integer,
+    'dischargeId': fields.String,
     'dischargeName': fields.String,
-    'dischargeAddress': fields.String,
-    # 'enter': fields.Nested(enter_detail_fields)
+    'dischargeShortName': fields.String,
+    'dischargeAddress': fields.String
 }
 
 discharge_list_fields = {
-    'total': fields.Integer(attribute=lambda pagination: pagination.total),
-    'currentPage': fields.Integer(attribute=lambda pagination: pagination.page),
-    'pageSize': fields.Integer(attribute=lambda pagination: pagination.per_page),
-    'hasNext': fields.Boolean(attribute=lambda pagination: pagination.has_next),
-    'list': fields.List(fields.Nested(discharge_item_fields), attribute=lambda pagination: pagination.items)
+    'total': fields.Integer(attribute=lambda p: p.total),
+    'currentPage': fields.Integer(attribute=lambda p: p.page),
+    'pageSize': fields.Integer(attribute=lambda p: p.per_page),
+    'hasNext': fields.Boolean(attribute=lambda p: p.has_next),
+    'list': fields.List(fields.Nested(discharge_item_fields), attribute=lambda p: p.items)
 }
 
 
 class DischargeResource(Resource):
+    decorators = [auth.login_required]
 
     @metric
-    @auth.login_required
     @marshal_with(discharge_detail_fields)
-    def get(self, discharge_id):
-        return Discharge.query.get_or_abort(discharge_id)
+    def get(self, discharge_id=None, monitor_id=None, report_id=None):
+        if discharge_id:
+            return Discharge.query.get_or_abort(discharge_id)
+        elif monitor_id:
+            return Monitor.query.get_or_abort(monitor_id).discharge
+        elif report_id:
+            return Report.query.get_or_abort(report_id).discharge
 
 
 class DischargeCollectionResource(Resource):
+    decorators = [auth.login_required]
 
     @metric
-    @auth.login_required
     @marshal_with(discharge_list_fields)
     def get(self, enter_id=None):
         parser = reqparse.RequestParser()
         parser.add_argument('currentPage', type=int, default=1)
         parser.add_argument('pageSize', type=int, default=20)
-        parser.add_argument('state', default=None)
         args = parser.parse_args()
         current_page = args.pop('currentPage')
         page_size = args.pop('pageSize')
@@ -63,10 +74,10 @@ class DischargeCollectionResource(Resource):
         else:
             query = Discharge.query.filter_by_user()
         return query.order_by(Discharge.dischargeId) \
-            .filter_by_state(args.pop('state')) \
             .filter_by_args(args) \
             .paginate(current_page, page_size, False)
 
 
-api.add_resource(DischargeResource, '/discharges/<int:discharge_id>')
+api.add_resource(DischargeResource, '/discharges/<int:discharge_id>', '/monitors/<int:monitor_id>/discharge',
+                 '/reports/<int:report_id>/discharge')
 api.add_resource(DischargeCollectionResource, '/discharges', '/enters/<int:enter_id>/discharges')
