29116c02adb972479e8d5bba1cf6b7206a0d7734
taoyimin <1103995288@qq.com>
2019-11-08 17:00:42 +0800

    add dictionary model

---
+++ app/model/dictionary.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author:Tao Yimin
+# Time  :2019/11/7 10:25
+from app.util.query import DictionaryQuery
+from . import db
+
+
+class Dictionary(db.Model):
+    """
+    数据字典实体类
+    attributes:
+        dictionaryId: 自增长主键
+    """
+    __table_args__ = {'schema': 'enterprise_archives.dbo'}
+    __tablename__ = 'T_Dictionary_Sub'
+    query_class = DictionaryQuery
+
+    dictionaryId = db.Column('Dic_Sub_Id', primary_key=True, autoincrement=True)
+    dictId = db.Column('Dic_Id')
+    dictCode = db.Column('Dic_Sub_Code')
+    dictName = db.Column('Dic_Sub_Name')
+    isDelete = db.Column('Is_Deleted', default=0)
+
+    __mapper_args__ = {
+        "order_by": dictionaryId
+    }
+
+    def __repr__(self):
+        return '<Dictionary %r>' % self.dictName
