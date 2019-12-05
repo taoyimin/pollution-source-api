b2cd0ed6b55bb1d7169c14ec002520d6a7be22cc
taoyimin <1103995288@qq.com>
2019-12-05 11:14:40 +0800

    add enter user class

--- app/model/user.py
+++ app/model/user.py
@@ -14,9 +14,61 @@ from app.model.report import DischargeReport, FactorReport
 from . import db, auth
 
 
-class User(db.Model):
-    # __bind_key__ = 'enterprise_home'
+class BaseUser:
+    """
+    所有类型用户的父类
+    继承验证token方法
+    """
+    @staticmethod
+    @auth.verify_token
+    def verify_token(token):
+        """
+        验证token
+        :param token:
+        :return:
+        """
+        if not token:
+            return False
+        g.user = None
+        s = Serializer(app.app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except SignatureExpired:
+            abort(401, message='token过期')
+            return False
+        except BadSignature as e:
+            abort(401, message='token无效')
+            return False
+        if data:
+            if data['type'] == app.app.config['ADMIN_USER_TYPE']:
+                # 环保用户
+                g.type = data['type']
+                g.user = AdminUser.query.get_or_404(data['id'], description='token验证错误，id=%d的环保用户不存在' % data['id'])
+                return True
+            elif data['type'] == app.app.config['ENTER_USER_TYPE']:
+                # 企业用户
+                g.type = data['type']
+                g.user = EnterUser.query.get_or_404(data['id'], description='token验证错误，id=%d的企业用户不存在' % data['id'])
+                return True
+            else:
+                abort(401, message='token验证错误，未知的用户类型！type=%d' % data['type'])
+        return False
 
+
+class AdminUser(db.Model, BaseUser):
+    """
+    环保用户实体类
+    attributes:
+        userId: 自增长主键
+        orgId: 组织id
+        userName: 用户名（不可重复）
+        realName: 真实名称
+        passWord: 明文密码
+        globalLevel:
+        userLevel:
+        isDelete: 是否删除
+        districts: 用户的管辖区域集合
+    """
     __table_args__ = {'schema': 'enterprise_home.dbo'}
     __tablename__ = 't_user'
 
@@ -38,7 +90,7 @@ class User(db.Model):
         self.__dict__.update(kwargs)
 
     def __repr__(self):
-        return '<User %r>' % self.userName
+        return '<AdminUser %r>' % self.userId
 
     def generate_auth_token(self, expiration=36000):
         """
@@ -46,35 +98,8 @@ class User(db.Model):
         :param expiration: 有效时间
         :return: token
         """
-        # s = Serializer(Config.SECRET_KEY, expiration)
         s = Serializer(app.app.config['SECRET_KEY'], expiration)
-        return s.dumps({'id': self.userId}).decode('utf-8')
-
-    @staticmethod
-    @auth.verify_token
-    def verify_token(token):
-        """
-        验证token
-        :param token:
-        :return:
-        """
-        if not token:
-            return False
-        g.user = None
-        # s = Serializer(Config.SECRET_KEY)
-        s = Serializer(app.app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token.encode('utf-8'))
-        except SignatureExpired:
-            abort(401, message='token过期')
-            return False
-        except BadSignature:
-            abort(401, message='token无效')
-            return False
-        if data:
-            g.user = User.query.get_or_404(data['id'], description='token验证错误，id=%d的用户不存在' % data['id'])
-            return True
-        return False
+        return s.dumps({'id': self.userId, 'type': app.app.config['ADMIN_USER_TYPE']}).decode('utf-8')
 
     @property
     def totalEnterCount(self):
@@ -136,20 +161,41 @@ class User(db.Model):
     def dealFactorReportCount(self):
         return FactorReport.query.filter_by_user().filter_by(state='0').count()
 
-    # def get_district_criterion(self):
-    #     """
-    #     获取用户管辖区域企业的过滤条件
-    #     :return:
-    #     """
-    #     district_code_list = map(lambda d: d.districtCode, self.districts)
-    #     if self.globalLevel == 'province':
-    #         if self.userLevel == '1':
-    #             return Enter.cityCode.in_(district_code_list)
-    #         if self.userLevel == '2':
-    #             return Enter.areaCode.in_(district_code_list)
-    #         if self.userLevel == '3':
-    #             return Enter.countyCode.in_(district_code_list)
-    #     if self.globalLevel == 'city' or 'county':
-    #         return Enter.areaCode.in_(district_code_list)
-    #     if self.globalLevel == 'industrialPark':
-    #         return Enter.countyCode.in_(district_code_list)
+
+class EnterUser(db.Model, BaseUser):
+    """
+    企业用户实体类
+    attributes:
+        userId: 自增长主键
+        enterId: 企业id
+        userName: 用户名（不可重复）
+        passWord: 明文密码
+        isDelete: 是否删除
+    """
+    __table_args__ = {'schema': 'enterprise_archives.dbo'}
+    __tablename__ = 't_Enterprise_Account'
+
+    userId = db.Column(db.Integer, name='id', primary_key=True, autoincrement=True)
+    enterId = db.Column('enter_id')
+    userName = db.Column('account_Name', unique=True)
+    passWord = db.Column('password_text')
+    isDelete = db.Column('is_deleted', default=0)
+
+    __mapper_args__ = {
+        "order_by": userId
+    }
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __repr__(self):
+        return '<EnterUser %r>' % self.userId
+
+    def generate_auth_token(self, expiration=36000):
+        """
+        生成token,默认有效时间10小时
+        :param expiration: 有效时间
+        :return: token
+        """
+        s = Serializer(app.app.config['SECRET_KEY'], expiration)
+        return s.dumps({'id': self.userId, 'type': app.app.config['ENTER_USER_TYPE']}).decode('utf-8')
