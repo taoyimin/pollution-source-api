0412ac76aea991fbf24968ed3fa3f21ef728e215
taoyimin <1103995288@qq.com>
2019-12-10 11:32:57 +0800

    add process post method

--- app/api/process.py
+++ app/api/process.py
@@ -72,38 +72,41 @@ class ProcessCollectionResource(Resource):
         return query.filter_by_args(args) \
             .paginate(current_page, page_size, False)
 
-    # @marshal_with(upload_return_fields)
-    # def post(self):
-    #     parser = reqparse.RequestParser()
-    #     parser.add_argument('operatePerson', required=True, type=valid_not_empty)
-    #     parser.add_argument('operateType', required=True, type=valid_not_empty)
-    #     parser.add_argument('operateDesc', required=True, type=valid_not_empty)
-    #     parser.add_argument('orderId', required=True, type=valid_not_empty)
-    #     parser.add_argument('file', type=werkzeug.FileStorage, location='files', required=False, action='append')
-    #     args = parser.parse_args()
-    #     if g.type == app.app.config['ADMIN_USER_TYPE']:
-    #         # 环保用户
-    #         args['operateUnit'] = g.user.globalLevel
-    #     elif g.type == app.app.config['ENTER_USER_TYPE']:
-    #         # 企业用户
-    #         args['operateUnit'] = 'company'
-    #     else:
-    #         abort(400, message='未知的用户类型，type=%d' % g.type)
-    #     files = args.pop('file')
-    #     report = Process(**args)
-    #     db.session.add(report)
-    #     db.session.flush()
-    #     if files:
-    #         for file in files:
-    #             args = save_file(file)
-    #             args['fileModelId'] = report.reportId
-    #             args['fileModel'] = app.app.config['DISCHARGE_REPORT_FILE_MODEL']
-    #             args['fileType'] = app.app.config['DISCHARGE_REPORT_FILE_TYPE']
-    #             attachment = Attachment(**args)
-    #             db.session.add(attachment)
-    #             db.session.flush()
-    #     db.session.commit()
-    #     return {'success': True, 'message': '提交成功'}
+    @marshal_with(upload_return_fields)
+    def post(self, order_id=None):
+        parser = reqparse.RequestParser()
+        parser.add_argument('operatePerson', required=True, type=valid_not_empty)
+        parser.add_argument('operateType', required=True, type=valid_not_empty)
+        parser.add_argument('operateDesc', required=True, type=valid_not_empty)
+        parser.add_argument('orderId', required=order_id is None, default=order_id, type=valid_not_empty)
+        parser.add_argument('file', type=werkzeug.FileStorage, location='files', required=False, action='append')
+        args = parser.parse_args()
+        if g.type == app.app.config['ADMIN_USER_TYPE']:
+            # 环保用户
+            args['operateUnit'] = g.user.globalLevel
+        elif g.type == app.app.config['ENTER_USER_TYPE']:
+            # 企业用户
+            args['operateUnit'] = 'company'
+        else:
+            abort(400, message='未知的用户类型，type=%d' % g.type)
+        args['operatePersonId'] = g.user.userId
+        files = args.pop('file')
+        if files:
+            ids = []
+            for file in files:
+                file_args = save_file(file)
+                file_args['fileModelId'] = args['orderId']
+                file_args['fileModel'] = app.app.config['ORDER_PROCESS_FILE_MODEL']
+                file_args['fileType'] = app.app.config['ORDER_PROCESS_FILE_TYPE']
+                attachment = Attachment(**file_args)
+                db.session.add(attachment)
+                db.session.flush()
+                ids.append(str(attachment.attachmentId))
+            args['attachmentIds'] = ','.join(ids)
+        process = Process(**args)
+        db.session.add(process)
+        db.session.commit()
+        return {'success': True, 'message': '提交成功'}
 
 
 api.add_resource(ProcessResource, '/processes/<int:process_id>')
