3917a858a1749f47f3dc12051cc59788e1ff2054
taoyimin <1103995288@qq.com>
2019-12-11 10:36:36 +0800

    modify logs path

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -9,8 +9,6 @@ import multiprocessing
 path_of_current_file = os.path.abspath(__file__)
 path_of_current_dir = os.path.split(path_of_current_file)[0]
 
-_file_name = 'log'
-
 sys.path.insert(0, path_of_current_dir)
 
 worker_class = 'sync'
@@ -29,6 +27,6 @@ reload = True
 debug = False
 
 bind = "%s:%s" % ("0.0.0.0", 5000)
-pidfile = '%s/run/%s.pid' % (path_of_current_dir, _file_name)
-errorlog = '%s/logs/%s_error.log' % (path_of_current_dir, _file_name)
-accesslog = '%s/logs/%s_access.log' % (path_of_current_dir, _file_name)
+pidfile = '%s/run/gunicorn.pid' % (path_of_current_dir)
+errorlog = '%s/logs/error.log' % (path_of_current_dir)
+accesslog = '%s/logs/access.log' % (path_of_current_dir)
